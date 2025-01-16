247730e5b92718ee2046549cb1d314b6c4e00724
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2025-01-16 01:34:42 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const consoleLang = 'pt';
 // nome da pasta onde as sessões serão salvas
 const credsFolder = 'sessions';
 // reiniciar o bot automaticamente se crashar
-const autoRestart = true;
+const autoRestart = false;
 /*
 * método de conexão
 * QR code : 'qr' | código de emparelhamento : 'pairing'
