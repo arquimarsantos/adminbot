288e6672cb87cd2899af7de96652524d04a8b7b5
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2025-01-16 22:12:52 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ app.get("/", function (req, res) {
     res.status(200).send("online!");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
